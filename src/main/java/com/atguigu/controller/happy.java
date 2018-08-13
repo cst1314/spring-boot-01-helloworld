02a76a3da244de511f2e957adac91157edbf66b7
cst1314 <41922054+cst1314@users.noreply.github.com>
2018-08-14 00:33:28 +0800

    updata happy.java meizizi

--- src/main/java/com/atguigu/controller/happy.java
+++ src/main/java/com/atguigu/controller/happy.java
@@ -3,7 +3,7 @@ package com.atguigu.controller;
 public class happy {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
+        
     }
 }
 

57bb41633b4156c8fd242144382f4b658c343315
tom_glb <goodMorning_glb@atguigu.com>
2018-08-14 02:06:23 +0800

    edit by localhost second

--- src/main/java/com/atguigu/controller/happy.java
+++ src/main/java/com/atguigu/controller/happy.java
@@ -2,8 +2,11 @@ package com.atguigu.controller;
 
 public class happy {
     public static void main(String[] args) {
+
+
+
         System.out.println("hello world edit by localhost ");
-        
+
     }
 }
 
